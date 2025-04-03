9d0d6e166182dad376ee5a74a7a8e8846e62b2fc
Dante0214 <rn_chic@naver.com>
2025-04-03 15:12:59 +0900

    feat: 서치바 수정

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,17 +5,14 @@ interface SearchBarProps {
 
 const SearchBar = ({ onSearch }: SearchBarProps) => {
   const [city, setCity] = useState("");
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSearch(city);
   };
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col items-center justify-center p-4"
-    >
+    <form onSubmit={handleSubmit}>
       <input
-        className="border rounded p-2 mr-2"
         type="text"
         placeholder="도시를 입력하세요"
         value={city}
@@ -23,12 +20,7 @@ const SearchBar = ({ onSearch }: SearchBarProps) => {
           setCity(e.target.value)
         }
       />
-      <button
-        type="submit"
-        className="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        검색
-      </button>
+      <button type="submit">검색</button>
     </form>
   );
 };
